15ef8d766ab10eddf4e713f85db60b6bfceea2a5
Joon kim <snsjoonkim@gmail.com>
2023-04-07 09:22:25 +0900

    Update for gradle7

--- src/src/test/java/io/utility/BasicTest.java
+++
@@ -1,9 +0,0 @@
-package io.utility;
-
-import java.nio.file.Paths;
-
-public abstract class BasicTest {
-
-	protected static String BASE_PATH = Paths.get(".").toAbsolutePath().normalize().toString();
-
-}
